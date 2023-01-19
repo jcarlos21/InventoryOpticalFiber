1aceb57f3e82efce5531a90f56258046b1545b9e
jcarlos21 <j.carlos2020@live.com>
2023-01-19 03:25:01 +0000

    Update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='netbox_invetory_fibers',
+    name='netbox_inventory_fibers,
     version='0.1',
     description='An example NetBox plugin',
     install_requires=[],
