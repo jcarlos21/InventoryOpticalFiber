b8a04aedcc9b502dc84921dfbfc12da5f2af6e8f
jcarlos21 <j.carlos2020@live.com>
2023-02-26 18:37:56 +0000

    update urls

--- netbox_inventory_fibers/urls.py
+++ netbox_inventory_fibers/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import models, views
 
+from netbox.views.generic import ObjectChangeLogView
+
 urlpatterns = (
 
     # Fornecedor
@@ -9,6 +11,9 @@ urlpatterns = (
     path('fornecedor/<int:pk>/', views.FornecedorView.as_view(), name='fornecedor'),
     path('fornecedor/<int:pk>/edit/', views.FornecedorEditView.as_view(), name='fornecedor_edit'),
     path('fornecedor/<int:pk>/delete/', views.FornecedorDeleteView.as_view(), name='fornecedor_delete'),
+    path('fornecedor/<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='fornecedor_changelog', kwargs={
+        'model': models.Fornecedor
+    }),
 
     # TipoBobina
     path('tipo-bobina/', views.TipoBobinaListView.as_view(), name='tipobobina_list'),
@@ -16,6 +21,9 @@ urlpatterns = (
     path('tipo-bobina/<int:pk>/', views.TipoBobinaView.as_view(), name='tipobobina'),
     path('tipo-bobina/<int:pk>/edit/', views.TipoBobinaEditView.as_view(), name='tipobobina_edit'),
     path('tipo-bobina/<int:pk>/delete/', views.TipoBobinaDeleteView.as_view(), name='tipobobina_delete'),
+    path('tipo-bobina/<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='tipobobina_changelog', kwargs={
+        'model': models.TipoBobina
+    }),
     
     # Bobina
     path('bobina/', views.BobinaListView.as_view(), name='bobina_list'),
@@ -23,6 +31,9 @@ urlpatterns = (
     path('bobina/<int:pk>/', views.BobinaView.as_view(), name='bobina'),
     path('bobina/<int:pk>/edit/', views.BobinaEditView.as_view(), name='bobina_edit'),
     path('bobina/<int:pk>/delete/', views.BobinaDeleteView.as_view(), name='bobina_delete'),
+    path('bobina/<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='bobina_changelog', kwargs={
+        'model': models.Bobina
+    }),
 
     # Requisicao
     path('requisicao/', views.RequisicaoListView.as_view(), name='requisicao_list'),
@@ -30,4 +41,7 @@ urlpatterns = (
     path('requisicao/<int:pk>/', views.RequisicaoView.as_view(), name='requisicao'),
     path('requisicao/<int:pk>/edit/', views.RequisicaoEditView.as_view(), name='requisicao_edit'),
     path('requisicao/<int:pk>/delete/', views.RequisicaoDeleteView.as_view(), name='requisicao_delete'),
+    path('requisicao/<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='requisicao_changelog', kwargs={
+        'model': models.Requisicao
+    }),
 )
