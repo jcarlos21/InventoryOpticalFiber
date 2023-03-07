690fd744cfe3b3f91ef4c95bc529277095fd7e33
jcarlos21 <j.carlos2020@live.com>
2023-03-07 02:20:52 +0000

    update API

---
+++ netbox_inventory_fibers/api/urls.py
@@ -0,0 +1,11 @@
+from netbox.api.routers import NetBoxRouter
+from . import views
+
+app_name = 'netbox_inventory_fibers'
+
+router = NetBoxRouter()
+router.register('fornecedor', views.FornecedorViewSet)
+router.register('tipo-bobina', views.TipoBobinaViewSet)
+router.register('bobina', views.BobinaViewSet)
+
+urlpatterns = router.urls
--- netbox_inventory_fibers/api/views.py
+++ netbox_inventory_fibers/api/views.py
@@ -1,5 +1,4 @@
 from netbox.api.viewsets import NetBoxModelViewSet
-
 from .. import filtersets, models
 from .serializers import FornecedorSerializer, TipoBobinaSerializer, QuantidadeFibraCaboSerializer, BobinaSerializer
 from django.db.models import Count
@@ -10,9 +9,19 @@ class FornecedorViewSet(NetBoxModelViewSet):
         bobinas_associadas=Count('bobinas_to_fornecedor')
     )
     serializer_class = FornecedorSerializer
+    filterset_class = filtersets.FornecedorFilterSet
 
 
 class TipoBobinaViewSet(NetBoxModelViewSet):
     queryset = models.TipoBobina.objects.prefetch_related('tags').annotate(
         bobinas_associadas=Count('bobinas')
     )
+    serializer_class = TipoBobinaSerializer
+
+
+class BobinaViewSet(NetBoxModelViewSet):
+    queryset = models.Bobina.objects.prefetch_related(
+        'nome_fornecedor', 'quantidade_fibras', 'tipo_bobina', 'tags'
+    )
+    serializer_class = BobinaSerializer
+    filterset_class = filtersets.BobinaFilterSet
